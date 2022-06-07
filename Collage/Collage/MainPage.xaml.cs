54ef7e16d47e3c6e61438d63b2d7b3f6ba1a4f52
Stage <Stage@KIK04>
2022-06-07 16:54:43 +0200

    Aggiungere i file di progetto.

---
+++ Collage/Collage/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Collage
+{
+    public partial class MainPage : ContentPage
+    {
+        private int index = 0;
+        private readonly String[] arrImg = { "https://www.fillmurray.com/500/500", "https://www.fillmurray.com/600/600", "https://www.fillmurray.com/800/800" };
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRight_Clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Equals(btnLeft))
+            {
+                if (index > 0)
+                {
+                    index--;
+                    image.Source = arrImg[index];
+                }
+            }
+            else if (button.Equals(btnRight))
+            {
+                if (index < arrImg.Length - 1)
+                {
+                    index++;
+                    image.Source = arrImg[index];
+                }
+            }
+        }
+    }
+}
